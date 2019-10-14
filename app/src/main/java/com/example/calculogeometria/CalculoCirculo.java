c771e4a1c1791c2693bdcc6e303cb34a6440efc0
Bector <patrickpoblete.g@gmail.com>
2019-10-14 01:19:53 -0300

    Proyecto terminado

--- app/src/main/java/com/example/calculogeometria/CalculoCirculo.java
+++ app/src/main/java/com/example/calculogeometria/CalculoCirculo.java
@@ -24,20 +24,12 @@ public class CalculoCirculo extends AppCompatActivity {
 
     public void btnCalcularRadio(View v){
         etRadio=(EditText) findViewById(R.id.etRadio);
-        try {
-            if (etRadio.getText().toString().isEmpty()){
-                Toast.makeText(getApplicationContext(),"El campo no debe estar vacío",Toast.LENGTH_LONG).show();
-            }else {
-                Intent intent2=new Intent(this, ResultadoCirculo.class);
-                intent2.putExtra("variable2", etRadio.getText().toString());
-                startActivity(intent2);
-            }
-        } catch (Exception e){
-            Toast.makeText(getApplicationContext(),"Error: ",Toast.LENGTH_SHORT).show();
+        if (etRadio.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"El campo no debe estar vacío",Toast.LENGTH_LONG).show();
+        }else {
+            Intent intent2=new Intent(this, ResultadoCirculo.class);
+            intent2.putExtra("variable2", etRadio.getText().toString());
+            startActivity(intent2);
         }
-
-
-
-
     }
 }
